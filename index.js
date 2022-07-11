7af2f137de155d732e5be78bf0eaf985f22f9fee
Mark Viti <+>
2022-07-10 20:18:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) { 
+    let array = []
+    for (let i = 0; i < names.length; i ++) {
+        array.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+
+    return array
+}
+
+function countDown( startingNumber ) { 
+    while ( startingNumber > 0) { 
+        console.log(startingNumber); 
+        startingNumber -= 1;
+    }
+    console.log( startingNumber);
+}
